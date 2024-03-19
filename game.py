c1f67da206901a46b8ea1b182575579e023f08f5
Ron M <rtm2702@gmail.com>
2024-03-19 13:25:43 +0100

    Added reset functionality, solved winner label bug

--- game.py
+++ game.py
@@ -6,6 +6,8 @@ class Game():
         pygame.init()
         self.p1 = player_one
         self.p2 = player_two
+        self.winner = ''
+        self.set_mode = ''
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         self.display_surface.fill(BG_COLOR)
         self.window_label = pygame.display.set_caption('CONNECT FOUR!')
@@ -53,14 +55,19 @@ class Game():
             for i in range(7):
                 if list(array[i, 0:4]) == sample_1 or list(array[i, 1:5]) == sample_1 or list(array[i, 2:]) == sample_1:
                     has_winner = True
+                    self.winner = 'player 1'
                 elif list(array[i, 0:4]) == sample_2 or list(array[i, 1:5]) == sample_2 or list(array[i, 2:]) == sample_2:
                     has_winner = True
+                    self.winner = 'player 2'
                     
             # check rows for possible winner
             for i in range(6):
-                if list(array[0:4, i]) == sample_1 or list(array[1:5, i]) == sample_1 or list(array[2:6, i]) == sample_1 or list(array[3:, i]) == sample_1 or \
-                    list(array[0:4, i]) == sample_2 or list(array[1:5, i]) == sample_2 or list(array[2:6, i]) == sample_2 or list(array[3:, i]) == sample_2:
+                if list(array[0:4, i]) == sample_1 or list(array[1:5, i]) == sample_1 or list(array[2:6, i]) == sample_1 or list(array[3:, i]) == sample_1:
                     has_winner = True
+                    self.winner = 'player 1'
+                elif list(array[0:4, i]) == sample_2 or list(array[1:5, i]) == sample_2 or list(array[2:6, i]) == sample_2 or list(array[3:, i]) == sample_2:
+                    has_winner = True
+                    self.winner = 'player 2'
                     
             # check diagonals for possible winner
             if list(np.diag(array))[0:4] == sample_1 or list(np.diag(array))[1:5] == sample_1 or list(np.diag(array))[2:] == sample_1 or \
@@ -72,6 +79,7 @@ class Game():
                list(np.diag(flipped_array, k=-1))[0:4] == sample_1 or list(np.diag(flipped_array, k=-1))[1:5] == sample_1  or list(np.diag(flipped_array, k=-1))[2:] == sample_1 or \
                list(np.diag(flipped_array, k=-2))[0:4] == sample_1 or list(np.diag(flipped_array, k=-2))[1:5] == sample_1 or list(np.diag(flipped_array, k=-3)) == sample_1:
                    has_winner = True
+                   self.winner = 'player 1'
             elif list(np.diag(array))[0:4] == sample_2 or list(np.diag(array))[1:5] == sample_2 or list(np.diag(array))[2:] == sample_2 or \
                  list(np.diag(array, k=1))[0:4] == sample_2 or list(np.diag(array, k=1))[1:5] == sample_2 or list(np.diag(array, k=2)) == sample_2 or \
                  list(np.diag(array, k=-1))[0:4] == sample_2 or list(np.diag(array, k=-1))[1:5] == sample_2  or list(np.diag(array, k=-1))[2:] == sample_2 or \
@@ -81,10 +89,10 @@ class Game():
                  list(np.diag(flipped_array, k=-1))[0:4] == sample_2 or list(np.diag(flipped_array, k=-1))[1:5] == sample_2  or list(np.diag(flipped_array, k=-1))[2:] == sample_2 or \
                  list(np.diag(flipped_array, k=-2))[0:4] == sample_2 or list(np.diag(flipped_array, k=-2))[1:5] == sample_2 or list(np.diag(flipped_array, k=-3)) == sample_2:
                     has_winner = True
+                    self.winner = 'player 2'
         return has_winner
         
     def intro(self):
-        set_mode = ''
         intro = True
         while intro:
             # intro text object
@@ -92,17 +100,17 @@ class Game():
             self.display_surface.blit(intro_text.render(), (CELL_WIDTH, CELL_HEIGHT))
             
             # instruction text object
-            instructions_line_one = Text(CELL_WIDTH * 7, CELL_HEIGHT, BG_COLOR, "Use LEFT and RIGHT arrow keys to choose column", "black", "Arial", 20)
+            instructions_line_one = Text(CELL_WIDTH * 7, CELL_HEIGHT, BG_COLOR, "Use LEFT and RIGHT arrow keys to choose column", "black", "Impact", 20)
             self.display_surface.blit(instructions_line_one.render(), (CELL_WIDTH * 2, CELL_HEIGHT * 3))
-            instructions_line_two = Text(CELL_WIDTH * 7, CELL_HEIGHT, BG_COLOR, "and DOWN arrow key to confirm choice.", "black", "Arial", 20)
+            instructions_line_two = Text(CELL_WIDTH * 7, CELL_HEIGHT, BG_COLOR, "and DOWN arrow key to confirm choice.", "black", "Impact", 20)
             self.display_surface.blit(instructions_line_two.render(), (CELL_WIDTH * 2, CELL_HEIGHT * 4))
             
             # play vs computer button object
-            vs_computer_btn = Button(CELL_WIDTH * 4, CELL_HEIGHT * 2, "deepskyblue4", "PLAY VS COMPUTER", "white", "Prequel Demo Shadow Italic", 22)
+            vs_computer_btn = Button(CELL_WIDTH * 4, CELL_HEIGHT * 2, "deepskyblue4", "PLAY VS COMPUTER", "white", "Impact", 22)
             self.display_surface.blit(vs_computer_btn.render(), (CELL_WIDTH, CELL_HEIGHT * 5))
             
             # play vs player 2 button object
-            vs_player_btn = Button(CELL_WIDTH * 4, CELL_HEIGHT * 2, "deepskyblue4", "PLAY VS PLAYER 2", "white", "Prequel Demo Shadow Italic", 22)
+            vs_player_btn = Button(CELL_WIDTH * 4, CELL_HEIGHT * 2, "deepskyblue4", "PLAY VS PLAYER 2", "white", "Impact", 22)
             self.display_surface.blit(vs_player_btn.render(), (CELL_WIDTH * 6, CELL_HEIGHT * 5))
             
             for event in pygame.event.get():
@@ -123,34 +131,27 @@ class Game():
             if pygame.mouse.get_pressed()[0]:
                 x, y = pygame.mouse.get_pos()
                 if (x >= CELL_WIDTH and x <= CELL_WIDTH * 5) and (y >= CELL_HEIGHT * 5 and y <= CELL_HEIGHT * 7):
-                    set_mode = "vs_computer"
+                    self.set_mode = "vs_computer"
                     intro = False
                 elif (x >= CELL_WIDTH * 6 and x <= CELL_WIDTH * 10) and (y >= CELL_HEIGHT * 5 and y <= CELL_HEIGHT * 7):
-                    set_mode = "vs_player"
+                    self.set_mode = "vs_player"
                     intro = False
                     
             pygame.display.update()
             self.clock.tick(FRAMERATE)
-        return set_mode
     
-    def run(self, set_mode):
+    def run(self):
         # mode set to 2 players
-        if set_mode == 'vs_player':
+        if self.set_mode == 'vs_player':
             i = 0
             player = 1
             column = 0
-            self.winner = ""
             while True:
                 self.display_surface.fill(BG_COLOR)
                 self.draw_board()
                 self.draw_coin()
                 if self.has_winner():
-                    if player == 1:
-                        self.winner = "player 2"
-                        break
-                    elif player == -1:
-                        self.winner = "player 1"
-                        break
+                    break
                 if player == 1:
                     self.display_surface.blit(self.p1.coin.render(), COLUMNS_CHOICE[i])
                 elif player == -1:
@@ -182,22 +183,16 @@ class Game():
                 pygame.display.update()
                 self.clock.tick(FRAMERATE)
                 
-        if set_mode == 'vs_computer':
+        if self.set_mode == 'vs_computer':
             i = 0
             player = 1
             column = 0
-            self.winner = ""
             while True:
                 self.display_surface.fill(BG_COLOR)
                 self.draw_board()
                 self.draw_coin()
                 if self.has_winner():
-                    if player == 1:
-                        self.winner = "computer"
-                        break
-                    elif player == -1:
-                        self.winner = "player 1"
-                        break
+                    break
                 if player == 1:
                     self.display_surface.blit(self.p1.coin.render(), COLUMNS_CHOICE[i])
                 elif player == -1:
@@ -236,23 +231,40 @@ class Game():
                 self.clock.tick(FRAMERATE)            
             
     def winner_screen(self):
-        while True:
+        restart = False
+        winner_screen = True
+        while winner_screen:
             winner_text = Text(CELL_WIDTH * 5, CELL_HEIGHT * 1, "deepskyblue4", "", "white", "Impact", 36)
             if self.winner == "player 1":
                 winner_text.text = "Player 1 won!"
-                self.display_surface.blit(winner_text.render(), (CELL_WIDTH * 3, CELL_HEIGHT * 1))
-            elif self.winner == "player 2":
+                self.display_surface.blit(winner_text.render(), (CELL_WIDTH * 2, CELL_HEIGHT * 1))
+            elif self.winner == "player 2" and self.set_mode == "vs_player":
                 winner_text.text = "Player 2 won!"
-                self.display_surface.blit(winner_text.render(), (CELL_WIDTH * 3, CELL_HEIGHT * 1))
-            elif self.winner == "computer":
+                self.display_surface.blit(winner_text.render(), (CELL_WIDTH * 2, CELL_HEIGHT * 1))
+            elif self.winner == "player 2" and self.set_mode == "vs_computer":
                 winner_text.text = "Computer won!"
-                self.display_surface.blit(winner_text.render(), (CELL_WIDTH * 3, CELL_HEIGHT * 1))
-                
+                self.display_surface.blit(winner_text.render(), (CELL_WIDTH * 2, CELL_HEIGHT * 1))
+            
+            restart_button = Button(CELL_WIDTH * 2, CELL_HEIGHT, "deepskyblue1", "Restart", "white", "Impact", 24)
+            self.display_surface.blit(restart_button.render(), (CELL_WIDTH * 7, CELL_HEIGHT))    
+            
+            x, y = pygame.mouse.get_pos()
+            if (x >= CELL_WIDTH * 7 and x <= CELL_WIDTH *9) and (y >= CELL_HEIGHT and y <= CELL_HEIGHT * 2):
+                restart_button.btn_color = "green3"
+                self.display_surface.blit(restart_button.render(), (CELL_WIDTH * 7, CELL_HEIGHT))
+            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-                    
+
+            if pygame.mouse.get_pressed()[0]:
+                x, y = pygame.mouse.get_pos()
+                if (x >= CELL_WIDTH * 7 and x <= CELL_WIDTH *9) and (y >= CELL_HEIGHT and y <= CELL_HEIGHT * 2):
+                    restart = True
+                    winner_screen = False
+
             pygame.display.update()
             self.clock.tick(FRAMERATE)
+        return restart
                     
--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from game import *
 
 # running the game
 if __name__ == '__main__':
+    # initializing standard objects
     coin_one = Coin(COIN_COLOR_ONE)
     coin_two = Coin(COIN_COLOR_TWO)
     player_one = Player(coin_one)
     player_two = Player(coin_two)
     game = Game(player_one, player_two)
-    set_mode = game.intro()
-    game.run(set_mode)
+    
+    game.intro()
+    game.run()
     game.winner_screen()
